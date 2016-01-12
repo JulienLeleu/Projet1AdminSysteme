d5baa70f7738eb98376af691b0f5d37ee8b6440b
jujupowaa <j.leleu@live.fr>
2016-01-12 21:37:23 +0100

    avancement dans ajout user

--- useradd.pl
+++ useradd.pl
@@ -9,7 +9,7 @@ my %user = (
 	gid => 50,
 	infos => undef,
 	home => '/home/',
-	bootProgram => '/bin/bash'
+	shell => '/bin/bash'
 );
 
 # Options
@@ -26,6 +26,7 @@ my $FILE_HELP = 'useradd.help';
 my $FILE_PASSWORD = 'passwd';
 my $FILE_GROUP = 'group';
 
+# Récupère les options et les rangent dans %args
 sub getArgs {
 	%args = @_;
 	#verification des arguments
@@ -36,7 +37,8 @@ sub getArgs {
 	}
 }
 
-sub containsOpt {
+# Retourne 1 si l'option est contenue dans le tableau passé en param
+sub containsOpt {	# @tab, $option
 	$option = $_[$#_];
 	for($i = 0; $i < $#_; $i++) {
 		if ($_[$i] eq $option) {
@@ -54,24 +56,72 @@ sub displayFile {
 	close(READER);
 }
 
-sub add {
+sub getUser {
 	# vérifier les options, les insérer dans user
+	foreach my $key (keys(%args)) {
+		$value = $args{$key};
+		# si l'option existe, on range la valeur dans l'utilisateur
+		if (defined $helpCommands{$key}) {
+			# on supprime le "--" devant l'option
+			$key =~ s/^--//;
+			$user{$key}=$value;
+		}
+	}
+	$user{login}=$_[0];
+	print "Ajout de l'utilisateur « $user{login} » ...\n";
+	if (!exists $args{"--home"}) {
+		$user{home} = $user{home} . $user{login};
+	}
+	print "Création du répertoire personnel « $user{home} »...\n";
+
+	$user{password} = getPassword();
+
 }
 
-if (containsOpt(@ARGV,'-h')) {
+sub getPassword {
+	system("stty -echo");
+	do {
+		if(defined $password1) {
+			print "Erreur : Mots de passes différents\n";
+		}
+		print "Entrez le nouveau mot de passe UNIX :\n";
+		$password1 = <STDIN>;
+		print "\n";
+		print "Retapez le nouveau mot de passe UNIX :\n";
+		$password2 = <STDIN>;
+		print "\n";
+	} while ($password1 ne $ password2);
+	system("stty echo");
+	return $password1;
+}
+
+# Si l'utilisateur fait ./useradd.pl --help
+if (containsOpt(@ARGV,'-h') or containsOpt(@ARGV,'--help')) {
 	system("clear");
 	displayFile($FILE_HELP);
 }
 else {
-	if ($ARGV[0] eq '-n') {
+	# Si l'utilisateur fait ./useradd.pl -n [options]
+	if (defined $ARGV[0] and $ARGV[0] eq '-n') {
 		shift;
 		foreach my $k (@ARGV) {
-			print "$helpCommands{$k}\n";
+			if (defined $helpCommands{$k}) {
+				print "$helpCommands{$k}\n";
+			}
+			else {
+				print "Option inconnue\n";
+			}
 		}
 	}
 	else {
+		# On récupère le login (dernier argument)
+		$login = pop @ARGV;
+		# Récupération des options
 		getArgs(@ARGV);
-		add();
+		# Récupère les infos
+		getUser($login);
+
+		## A supprimer
 		foreach my $k (keys(%args)) {
 			print "$k ==> $args{$k}\n";
 		}
